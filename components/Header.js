e9806251eb5079e998af84e5d232a5dc0d3aaba0
Paulo Tejada <paulotejada.123a@gmail.com>
2021-06-27 17:26:13 -0500

    Auth Context

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,9 @@
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
 import Link from 'next/link'
 import Search from '@/components/Search'
 import styles from '@/styles/Header.module.css'
+
+
 
 export default function Header() {
   return (
@@ -22,6 +25,13 @@ export default function Header() {
           <li>
             <Link href='/events/add'>
               <a>Add Event</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/account/login'>
+              <a className='btn-secondary btn-icon'>
+                <FaSignInAlt />
+                Login</a>
             </Link>
           </li>
         </ul>
